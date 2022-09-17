ff266fa0db3196d9e87dbb3a8c3bf59fbd395b0b
ich5nkn <kogaktt@gmail.com>
2022-09-17 21:56:41 +0900

    fix: set base url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: 'https://ich5nkn.github.io/splatoon2and3-merged-schedule',
+  basePath: 'https://ich5nkn.github.io/splatoon2and3-merged-schedule',
 }
 
 module.exports = nextConfig
