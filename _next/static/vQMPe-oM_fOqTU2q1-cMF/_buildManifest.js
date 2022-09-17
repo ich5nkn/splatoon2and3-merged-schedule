a86903e13d7ca79adf469fc725dbde4183a1bc75
ich5nkn <ich5nkn@users.noreply.github.com>
2022-09-17 17:59:51 +0000

    deploy: 4a8edf1c1b2265dffdf715de6d240ee37407018e

---
+++ _next/static/vQMPe-oM_fOqTU2q1-cMF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/be3713e878499362.css","static/chunks/pages/index-d032f091b4637989.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
