37c537849f4148b50d1e7aa83172f8f9c08b5a5c
ich5nkn <kogaktt@gmail.com>
2022-09-17 23:16:53 +0900

    fix: image path

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
             <meta property="og:site_name" content="splatoon2,3 Schedule" />
             <meta property="og:description" content="Splatoon2とSplatoon3のスケジュールを同時に見ることができます。イカリング２とイカリング３を行ったり来たりしなくてよくなります！個人用に作成しましたが、ルールとステージ次第ではスプラ２のパワー更新も狙いたいという方のお役に立つかもです。" />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content="ogp.png" />
+            <meta property="og:image" content="https://ich5nkn.github.io/splatoon2and3-merged-schedule/ogp.png" />
             <meta name="twitter:card" content="summary_large_image" />
         </Head>
         <body>
