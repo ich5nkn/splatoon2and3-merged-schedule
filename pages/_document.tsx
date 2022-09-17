18c1ab83340004cfedc4cfb3b3847b180907e5ca
ich5nkn <kogaktt@gmail.com>
2022-09-17 22:59:11 +0900

    feat: ogp setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html >
-        <Head prefix="https://ich5nkn.github.io/splatoon2and3-merged-schedule"/>
+        <Head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#'>
+            <meta property="og:url" content="https://ich5nkn.github.io/splatoon2and3-merged-schedule" />
+            <meta property="og:title" content="splatoon2,3 Schedule" />
+            <meta property="og:site_name" content="splatoon2,3 Schedule" />
+            <meta property="og:description" content="Splatoon2とSplatoon3のスケジュールを同時に見ることができます。イカリング２とイカリング３を行ったり来たりしなくてよくなります！個人用に作成しましたが、ルールとステージ次第ではスプラ２のパワー更新も狙いたいという方のお役に立つかもです。" />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content="ogp.png" />
+            <meta name="twitter:card" content="summary_large_image" />
+        </Head>
         <body>
           <Main />
           <NextScript />
